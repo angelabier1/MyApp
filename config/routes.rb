d5ae58e16eefd8df3728427c985ec582a09c8b4b
Angela Bier <abier@corelando.com>
2015-02-18 14:47:17 -0500

    rails_apps_composer: add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'visitors#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
